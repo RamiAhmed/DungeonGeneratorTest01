db4f30c0387f374374a7e0f0fd6eea02084591ad
Rami Ahmed Bock <ramiahmedbock@gmail.com>
2020-10-02 00:21:09 +0200

    base class for (local) player dependent systems

---
+++ Assets/Core/Player/BasePlayerDependentSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace Assets.Core.Player
+{
+    public abstract class BasePlayerDependentSystem : SystemBase
+    {
+        private Entity _player;
+
+        protected Entity LocalPlayer => _player == Entity.Null ? (_player = GetPlayerEntity(1)) : _player;
+
+        protected override void OnCreate()
+        {
+            UnityEngine.Debug.Log($"{this}: OnCreate");
+        }
+
+        protected override void OnStartRunning()
+        {
+            UnityEngine.Debug.Log($"{this}: OnStartRunning");
+        }
+
+        protected Entity GetPlayerEntity(int index)
+        {
+            var query = GetEntityQuery(ComponentType.ReadOnly<PlayerComponentData>());
+            using var entities = query.ToEntityArray(Allocator.TempJob);
+
+            return entities.SingleOrDefault(entity => EntityManager.GetComponentData<PlayerComponentData>(entity).Index == index);
+        }
+    }
+}
