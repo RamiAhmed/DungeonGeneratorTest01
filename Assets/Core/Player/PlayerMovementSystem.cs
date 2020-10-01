e45c1eac226aea68d6a21c1f0b25713338672bcb
Rami Ahmed Bock <ramiahmedbock@gmail.com>
2020-10-02 00:21:22 +0200

    player movement system

---
+++ Assets/Core/Player/PlayerMovementSystem.cs
@@ -0,0 +1,31 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+
+namespace Assets.Core.Player
+{
+    [UpdateAfter(typeof(PlayerInputSystem))]
+    public class PlayerMovementSystem : SystemBase
+    {
+        protected override void OnCreate()
+        {
+            UnityEngine.Debug.Log($"{this}: OnCreate");
+        }
+
+        protected override void OnStartRunning()
+        {
+            UnityEngine.Debug.Log($"{this}: OnStartRunning");
+        }
+
+        protected override void OnUpdate()
+        {
+            var deltaTime = Time.DeltaTime;
+            Entities
+                .WithName("PlayerMovementSystem_ForEach")
+                .ForEach((ref Translation translation, in PlayerMovementComponentData playerMove) =>
+                {
+                    translation.Value += playerMove.Velocity * deltaTime;
+                })
+                .ScheduleParallel();
+        }
+    }
+}
