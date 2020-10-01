4e04c14c3a9292f4e2b0c8d44ffe692f878971fc
Rami Ahmed Bock <ramiahmedbock@gmail.com>
2020-10-02 00:20:53 +0200

    player input system

---
+++ Assets/Core/Player/PlayerInputSystem.cs
@@ -0,0 +1,33 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace Assets.Core.Player
+{
+    [UpdateAfter(typeof(PlayerCreationSystem))]
+    public class PlayerInputSystem : BasePlayerDependentSystem
+    {
+        protected override void OnUpdate()
+        {
+            var velocity = Vector3.zero;
+            if (Input.GetKey(KeyCode.W))
+            {
+                velocity.z += 1f;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                velocity.z -= 1f;
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                velocity.x -= 1f;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                velocity.x += 1f;
+            }
+
+            EntityManager.SetComponentData(LocalPlayer, new PlayerMovementComponentData { Velocity = velocity });
+        }
+    }
+}
